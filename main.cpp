a306bb23e22573d55e3db5601638f155b9fd6fd4
abdulrahmanshalaby <117514851+abdulrahmanshalaby@users.noreply.github.com>
2024-04-24 12:48:56 +0200

    class Player added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include<fstream>
+#include<vector>
+#include<String>
+using namespace std;
+class Player{
+    string name;
+    int year_of_birth;
+    int height;
+    int int_goals;
+    string nationality;
+public:
+     void set_name(string n){
+         name=n;
+     }
+    void set_yearofbirth(int n){
+        year_of_birth=n;
+    }
+    void set_intgoals(int n){
+         int_goals-n;
+     }
+    void set_height(int n){
+        height=n;
+    }
+    void set_nationality(string n){
+        nationality=n;
+    }
+  string get_name(){
+         return name;
+     }
+    int get_yearofbirth(){
+        return year_of_birth;
+    }
+    int get_intgoals(){
+        return int_goals;
+    }
+    int get_height(){
+        return height;
+    }
+    string get_nationality(){
+        return nationality;
+    }
+};
+class NationalTeam{
+Player players[18];
+bool active[18]={false};
 
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
