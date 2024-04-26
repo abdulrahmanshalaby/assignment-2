64afdd850fca8b12fd2b5befe0b875a5ef4e4e97
abdulrahmanshalaby <117514851+abdulrahmanshalaby@users.noreply.github.com>
2024-04-26 16:35:24 +0300

    class NationalTeam finished

--- main.cpp
+++ main.cpp
@@ -51,42 +51,169 @@ public:
     }
 };
 class NationalTeam{
+    string country;
     static int i;
 Player players[18];
 bool active[18]={false};
 public:
+    NationalTeam(string country){
+        this->country=country;
+    }
 bool addPlayer(const Player&obj){
     if(i>18){
-        cout<<"team full";
+        cout<<"team full"<<endl;
         return false;
     }
     players[i]=obj;
     active[i]=true;
-    cout<<"player number "<<i+1<<" added ";
+    cout<<"player number "<<i+1<<" added "<<endl;
     i++;
     return true;
 }
 bool removeplayer(string n){
     bool flag;
-    for (int j = 0; j < sizeof (players); ++j) {
+    for (int j = 0; j < 18; ++j) {
         if(players[j].get_name()==n){
             active[j]=false;
             flag=true;
         }
 
     }
-    if(flag)
-        return true;
-    return false;
+    if(flag){
+        cout<<"player(S) with name "<<n<<" where removed from team"<<endl;
+        return true;}
+    else {
+        cout<<"No player with that name"<<endl;
+        return false;}
 }
+int getNumOfPlayers(){
+    int i=0;
+    for (int j = 0; j < 18; ++j) {
+        if(active[j]==true)
+            i++;
+    }
+    return i;
+}
+int AvgHeight(){
+    int i=0;
+    int k=0;
+    for (int j = 0; j < 18; ++j) {
+        if (active[j] == true) {
+            i++;
+        }
+    }
+        for (int l = 0; l < 18; ++l) {
+            if (i==0)
+                return i;
+            k=k+(players[l].get_height()/i);
+        }
+
+
+    return k;
+}
+Player OldestPlayer(){
+    int k=0;
+    long long oldest=57857824758;
+    for (int j = 0; j < 18; ++j) {
+        if(active[j]== true){
+        if(players[j].get_yearofbirth()<oldest) {
+            oldest = players[j].get_yearofbirth();
+            k = j;
+        }
+        }
+
+    }
+    return players[k];
+}
+Player YoungestPlayer(){
+        int k=0;
+        int oldest=0;
+        for (int j = 0; j < 18; ++j) {
+            if(active[j]== true){
+                if(players[j].get_yearofbirth()>oldest) {
+                    oldest = players[j].get_yearofbirth();
+                    k = j;
+                }
+            }
+
+        }
+        return players[k];
+    }
+
+Player HighestIntGoals(){
+    int k=0;
+    int Hgoals=0;
+    for (int j = 0; j < 18; ++j) {
+        if(active[j]== true){
+            if(players[j].get_intgoals()>Hgoals) {
+                Hgoals = players[j].get_intgoals();
+                k = j;
+            }
+        }
+
+    }
+    return players[k];
+}
+ friend ostream& operator <<(ostream&os,NationalTeam& obj);
+    void writeStatistics(ofstream& ofs) {
+        ofs << "Team Country: " << country << endl;
+        ofs << "Number of players: " << getNumOfPlayers() << endl;
+        ofs << "Average height of players: " << AvgHeight() << endl;
+
+        Player oldest = OldestPlayer();
+        ofs << "Name and age of oldest player: " << oldest.get_name() << " (" << (2024 - oldest.get_yearofbirth()) << " years old)" << endl;
+
+        Player youngest = YoungestPlayer();
+        ofs << "Name and age of youngest player: " << youngest.get_name() << " (" << (2024 - youngest.get_yearofbirth()) << " years old)" << endl;
+
+        ofs << "Average number of international goals of players: " << AvgIntGoals() << endl;
+
+        Player highestGoals =HighestIntGoals() ;
+        ofs << "Player with the highest international goals: " << highestGoals.get_name() << " (" << highestGoals.get_intgoals() << " goals)" << endl;
+    }
+
+    int AvgIntGoals(){
+        int i=0;
+        int k=0;
+        for (int j = 0; j < 18; ++j) {
+            if (active[j] == true) {
+                i++;
+            }
+        }
+        for (int l = 0; l < 18; ++l) {
+            if (i==0)
+                return i;
+            k=k+(players[l].get_intgoals()/i);
+        }
+
+
+        return k;
+    }
+
 };
 int NationalTeam::i=0;
+ostream& operator <<(ostream&os,NationalTeam& obj){
+    os<<"team country: "<<obj.country<<endl;
+    for (int i = 0; i < 18; ++i) {
+        if(obj.active[i]== true){
+            os<<"player "<<i+1<<" "<<"info:"<<endl;
+            os<<obj.players[i].get_name()<<endl;
+        }
+
+    }
+    return os;
+}
 int main() {
-Player a("ahmed",22,180,22,"egyptian");
-    Player b("ahmed",22,180,22,"egyptian");
-    NationalTeam ahly;
+Player a("ahmed",2004,180,22,"egyptian");
+    Player b("ahme",2005,180,22,"egyptian");
+    Player c("ramy",2005,180,22,"egyptian");
+
+    NationalTeam ahly("egypt");
     ahly.addPlayer(a);
     ahly.addPlayer(b);
-    ahly.removeplayer("ahmed");
+    ahly.addPlayer(c);
+ofstream out("out.txt");
+ahly.writeStatistics(out);
+
 
 }
